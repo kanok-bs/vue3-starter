a702b226b152074c4d54adf225545704ceb04629
kanok-bs <kanok@brainstation-23.com>
2021-10-18 20:00:08 +0600

    fixed add user and user list view

--- src/components/users/UserAdd.vue
+++ src/components/users/UserAdd.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import useNotyf from "./../../utils/useNotyf";
+import Multiselect from "@vueform/multiselect";
 const notyf = useNotyf();
 import { defineProps, reactive, toRefs } from "vue";
 import { user } from "@/models/user";
@@ -12,6 +13,9 @@ const props = defineProps({
   countries: {
     type: Array,
   },
+  borders: {
+    type: Array,
+  },
 });
 const input = reactive<user>({} as any);
 const Add = () => {
@@ -20,7 +24,7 @@ const Add = () => {
   notyf.success("Save Succesfull");
 };
 </script>
-
+<style src="@vueform/multiselect/themes/default.css"></style>
 <template>
   <form @submit.prevent="Add">
     <div class="form-row">
@@ -28,13 +32,41 @@ const Add = () => {
         <label for="inputEmail4">Name</label>
         <input type="text" class="form-control" v-model="input.name" />
       </div>
-      <div class="form-group col-md-4">
+      <div class="form-group col-md-6">
         <label for="inputState">Countries</label>
         <select class="form-control" v-model="input.countries">
-          <option v-for="c in props.countries" :key="c.name" :value="c">
+          <option v-for="c in props.countries" :key="c.name" :value="c.name">
             {{ c.name }}
           </option>
         </select>
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputState">Borders</label>
+        <Multiselect
+          label="value"
+          v-model="input.borders"
+          placeholder="Select your borders"
+          :options="borders"
+          mode="multiple"
+          :closeOnSelect="false"
+        >
+        </Multiselect>
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputEmail4">Currency</label>
+        <input type="text" class="form-control" v-model="input.currency" />
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputEmail4">Language</label>
+        <input type="text" class="form-control" v-model="input.language" />
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputEmail4">Region</label>
+        <input type="text" class="form-control" v-model="input.region" />
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputEmail4">Capital</label>
+        <input type="text" class="form-control" v-model="input.capital" />
       </div>
     </div>
     <div class="from-group mt-4">
